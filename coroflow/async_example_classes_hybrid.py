1c7f8586bd5295d91e9d0a24575d12c2bb50f3ce
Dewald Abrie <dewald@rentalcover.com>
2020-11-05 11:12:47 +1100

    Example of sync and async hybrid operation

---
+++ coroflow/async_example_classes_hybrid.py
@@ -0,0 +1,48 @@
+from coroflow import Task, Pipeline
+import asyncio
+import time
+
+
+class GenTask(Task):
+    @staticmethod
+    def inner(context, inpt):
+        for url in ['img_url_1', 'img_url_2', 'img_url_3']:
+            print(f"Yielding {url}")
+            time.sleep(1)
+            yield url
+        print("Generator is exhausted")
+        return
+
+
+class Task1(Task):
+    @staticmethod
+    async def inner(context, inpt, param=None):
+        # do your async pipelined work
+        await asyncio.sleep(1)  # simulated IO delay
+        outp = inpt
+        print(f"func1: T1 sending {inpt}")
+        return outp
+
+
+class Task2(Task):
+    @staticmethod
+    def inner(context, inpt, param=None):
+        print(f"func2: T2 processing {inpt}")
+        time.sleep(1)  # simulated IO delay
+        outp = inpt
+        return outp
+
+
+p = Pipeline()
+t0 = GenTask('gen', p)
+t1 = Task1('func1', p, kwargs={'param': 'param_t1'})
+t2 = Task2('func2', p, kwargs={'param': 'param_t2'})
+t0.set_downstream(t1)
+t1.set_downstream(t2)
+
+
+# %%
+start_time = time.time()
+p.run(render=True, show_queues=True)
+print(f"Asynchronous duration: {time.time() - start_time}s.")
+
