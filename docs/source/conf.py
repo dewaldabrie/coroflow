d59dfa0014d0cbe25e61075aa8c56457ff3acc21
Dewald Abrie <dewald@rentalcover.com>
2021-07-12 22:53:50 +1000

    Bump to version 4.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Dewald Abrie'
 author = 'Dewald Abrie'
 
 # The full version, including alpha/beta/rc tags
-release = '3.3.3'
+release = '4.0.0'
 
 
 # -- General configuration ---------------------------------------------------
